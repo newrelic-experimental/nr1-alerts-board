5186358e8162996da58c08a1d47888f7137832a4
Abhishek Lad <abhisheklad12@gmail.com>
2020-01-13 12:32:38 +1100

    refactored
    Removed components to simplify.

--- nerdlets/alerts-dashboard-nerdlet/alerts-dashboard.js
+++ nerdlets/alerts-dashboard-nerdlet/alerts-dashboard.js
@@ -1,14 +1,123 @@
 import React from "react";
 import { Stack, StackItem, Grid, GridItem } from "nr1";
+import { EntitiesByDomainTypeQuery } from "nr1";
+import { Card, Popup, Image } from "semantic-ui-react";
+import Critical from "./assets/CRITICAL.png";
+import Warning from "./assets/WARNING.png";
+import NotAlerting from "./assets/NOT_ALERTING.png";
+import NotConfigured from "./assets/NOT_CONFIGURED.png";
 import AccountPicker from "./account-picker.js";
-import EntitySearch from "./entities-search.js";
+
 export default class AlertsDashboard extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      entities: []
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    EntitiesByDomainTypeQuery.query({
+      entityDomain: "APM",
+      entityType: "APPLICATION"
+    }).then(({ data }) => {
+      console.log("DATA ENTITIES");
+      console.log(data.entities);
+      let sortedArray = this.sortbySeverity(data.entities);
+      this.setState({ entities: data.entities });
+      console.log("STATE");
+      console.log(this.state.entities);
+    });
+  }
+  sortbySeverity(data) {
+    var severtityOrder = [
+      "CRITICAL",
+      "WARNING",
+      "NOT_ALERTING",
+      "NOT_CONFIGURED"
+    ];
+    let sortedData = data.sort((a, b) => {
+      return (
+        severtityOrder.indexOf(a.alertSeverity) -
+        severtityOrder.indexOf(b.alertSeverity)
+      );
+    });
+    console.log("SORTED");
+    console.log(sortedData);
+  }
+
+  rendercards() {
+    return (
+      <div>
+        <Card.Group style={{ margin: "auto", width: "100%" }} centered>
+          {this.state.entities.map((entity, i) => {
+            let appLink =
+              "https://one.newrelic.com/redirect/entity/" + entity.guid;
+            return (
+              <Card color={this.setColor(entity.alertSeverity)} key={i}>
+                <Card.Content>
+                  <Image
+                    alt={entity.alertSeverity}
+                    floated="right"
+                    size="mini"
+                    src={this.setLogo(entity.alertSeverity)}
+                  />
+                  <Card.Header>
+                    {
+                      <Popup
+                        hoverable
+                        position="top center"
+                        content={
+                          <a
+                            href={appLink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {entity.name}
+                          </a>
+                        }
+                        trigger={<h5>{entity.name}</h5>}
+                      />
+                    }
+                  </Card.Header>
+                </Card.Content>
+              </Card>
+            );
+          })}
+        </Card.Group>
+      </div>
+    );
+  }
+
+  setColor(alertSeverity) {
+    switch (alertSeverity) {
+      case "CRITICAL":
+        return "red";
+      case "NOT_ALERTING":
+        return "green";
+      case "NOT_CONFIGURED":
+        return "grey";
+      case "WARNING":
+        return "orange";
+      default:
+        return "white";
+    }
+  }
+
+  setLogo(alertSeverity) {
+    switch (alertSeverity) {
+      case "CRITICAL":
+        return Critical;
+      case "NOT_ALERTING":
+        return NotAlerting;
+      case "NOT_CONFIGURED":
+        return NotConfigured;
+      case "WARNING":
+        return Warning;
+      default:
+        return "";
+    }
+  }
 
   render() {
     return (
@@ -33,6 +142,18 @@ export default class AlertsDashboard extends React.Component {
               </StackItem>
             </Stack>
           </StackItem>
+          <StackItem className="toolbar-section2">
+            <Stack
+              fullWidth
+              fullHeight
+              verticalType={Stack.VERTICAL_TYPE.CENTER}
+              horizontalType={Stack.HORIZONTAL_TYPE.RIGHT}
+            >
+              <StackItem>
+                <h1>this is placeholder</h1>
+              </StackItem>
+            </Stack>
+          </StackItem>
         </Stack>
         <Grid
           className="primary-grid"
@@ -45,7 +166,7 @@ export default class AlertsDashboard extends React.Component {
       */}
           <GridItem className="primary-content-container" columnSpan={12}>
             <main className="primary-content full-height">
-              <EntitySearch></EntitySearch>
+              {this.rendercards()}
             </main>
           </GridItem>
         </Grid>
